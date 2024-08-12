109de62a9673798be6484f0be2b3ae0cdc1485cd
Krushovice <kickstar69@yandex.ru>
2024-08-12 18:39:39 +1000

    create Animal model

--- tasky/pet/models.py
+++ tasky/pet/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=25, default="Dog")
+    age = models.IntegerField()
+    owner_first_name = models.CharField(max_length=50)
+    owner_last_name = models.CharField(max_length=50)
+    owner_phone_number = PhoneNumberField(region="RU")
+    special_signs = models.CharField(max_length=100)
