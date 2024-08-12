332d8bb4a79cf6599248911f11188ffa8eb008f9
Krushovice <kickstar69@yandex.ru>
2024-08-12 19:01:04 +1000

    add pet directory to templates

--- tasky/pet/views.py
+++ tasky/pet/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
-from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class PetIndexView(View):
+    def get(self, request, *args, **kwargs):
+        animal_photo_url = (
+            "https://i.natgeofe.com/n/9135ca87-0115-4a22-8caf-d1bdef97a814/75552.jpg"
+        )
+        return render(
+            request,
+            "pets/index.html",
+            context={
+                "text": "It's the animal which we're find last",
+                "url": animal_photo_url,
+            },
+        )
