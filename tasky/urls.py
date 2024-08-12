f747906ce68d464f97b0571096dd4424ef845069
Krushovice <kickstar69@yandex.ru>
2024-08-12 18:40:19 +1000

    first steps with pet app

--- tasky/urls.py
+++ tasky/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('pet/', include("tasky.pet.urls")),
-    path('admin/', admin.site.urls),
+    path("", views.IndexView.as_view(), name="index"),
+    path("pet/", include("tasky.pet.urls")),
+    path("admin/", admin.site.urls),
 ]
