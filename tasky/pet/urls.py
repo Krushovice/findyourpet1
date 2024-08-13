8d434fd8403bb743cc25dd0d8d8f5eba5121c68e
Krushovice <kickstar69@yandex.ru>
2024-08-13 18:22:26 +1000

    build url for each animal

--- tasky/pet/urls.py
+++ tasky/pet/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.PetIndexView.as_view(), name="pet_index"),
+    path("<int:pk>/", views.PetShowView.as_view(), name="pet_show"),
 ]
--- tasky/pet/utils.py
+++ tasky/pet/utils.py
@@ -1,6 +1,27 @@
+from faker import Faker
+
 from tasky.pet.models import Animal
 
 
 def generate_url(animal_id: int) -> str:
     base_url = "http://127.0.0.1:8000/"
     return f"{base_url}/pet/{animal_id}"
+
+
+def generate_animal_description(
+    faker: Faker,
+    animals: list,
+) -> str:
+    animal_type = faker.random_element(
+        elements=animals,
+    )
+
+    characteristic1 = faker.random_element(
+        elements=("big", "small", "fast", "slow", "colorful", "striped")
+    )
+    characteristic2 = faker.random_element(
+        elements=("long", "short", "fluffy", "scaly", "feathered", "slimy")
+    )
+
+    description = f"The beautiful {animal_type}, {characteristic1} pet with {characteristic2} wool."
+    return description
